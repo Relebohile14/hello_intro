e20fb7c73b94033f089f90b7dd12ddf6c9d2e227
Relebohile Bill <majakerelebohile947@gmail.com>
2025-06-14 16:06:44 -0700

    Initial commit: Hello World + Personal Intro Script

---
+++ hello_into.py
@@ -0,0 +1,19 @@
+#Project: “Hello World + Personal Intro” Script
+#Print your name
+name = "Relebohile Bill"
+print(f"Name: {name}")
+
+#Print today’s date
+import datetime
+date = datetime.datetime.now()
+print(f"Today's date: {date}")
+
+#Print your goal for learning Python
+goal = "Being a master in python"
+print(f"My goal for python: {goal}")
+
+#Print your favorite quote
+quote = "When the time is right, I the Lord will make it happen"
+print(f"Favoutite quote: {quote}")
+
+#Save file as: hello_intro.py
---
+++ variables_intro.py
@@ -0,0 +1,18 @@
+#TASK 3
+#Write a Python script that:
+
+#Defines your name, age, and favorite subject as variables
+name = "Relebohile Bill"
+print (f"Name: {name}")
+
+age = 20
+print(f"Age: {age}")
+
+
+#Calculates your age in months
+current_year = 2025
+year_born = input(int("Enter your age: "))
+age = (current_year - int(year_born))
+print(age)
+
+#Prints a sentence using string formatting
